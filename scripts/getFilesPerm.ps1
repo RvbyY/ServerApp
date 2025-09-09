e32cb99a521ef354aacb5660efb5d8dae1d3a3ef
Bassirou TALL <107211867+RvbyY@users.noreply.github.com>
2025-09-09 16:31:56 +0200

    build: getFilesPerm, function in ps1 scripts that list in a recursive way, files info w/ users that could access to them

---
+++ scripts/getFilesPerm.ps1
@@ -0,0 +1,18 @@
+function getFilePerm
+{
+    $files = Get-ChildItem -Recurse
+
+    if (!$files) {
+        Write-Host "No files found" -ForegroundColor Red
+    }
+    foreach ($file in $files) {
+        Write-Host "$($file.Mode)   $($file.LastWriteTime)    $($file.Length)   $($file.Name)"
+        $rights = Get-Acl $file
+        $rights = $rights.Access | Select-Object IdentityReference
+        foreach ($right in $rights) {
+            Write-Host "$($right)" -ForegroundColor Blue
+        }
+    }
+}
+
+getFilePerm
